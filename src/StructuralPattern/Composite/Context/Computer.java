49f7457355320cb7d05f97d282a3ea02991d8edc
anylogic_국승현 <cooksh2421>
2020-02-26 14:14:04 +0900

    Composite패턴 완료

--- src/StructuralPattern/Composite/Context/Computer.java
+++ src/StructuralPattern/Composite/Context/Computer.java
@@ -4,6 +4,7 @@ public class Computer {
     private Keyboard keyboard;
     private Body body;
     private Monitor monitor;
+    private Speaker speaker;  // 추가
 
     public void addKeyboard(Keyboard keyboard) {
         this.keyboard = keyboard;
@@ -17,17 +18,21 @@ public class Computer {
         this.monitor = monitor;
     }
 
+    public void addSpeaker(Speaker speaker) { this.speaker = speaker; }  // 추가
+
     public int getPrice() {
         int keyboardPrice = keyboard.getPrice();
         int bodyPrice = body.getPrice();
         int monitorPrice = monitor.getPrice();
-        return keyboardPrice + bodyPrice + monitorPrice;
+        int speakerPrice = speaker.getPrice();  // 추가
+        return keyboardPrice + bodyPrice + monitorPrice + speakerPrice;
     }
 
     public int getPower() {
         int keyboardPower = keyboard.getPower();
         int bodyPower = body.getPower();
         int monitorPower = monitor.getPower();
-        return keyboardPower + bodyPower + monitorPower;
+        int speakerPower = speaker.getPower();  // 추가
+        return keyboardPower + bodyPower + monitorPower + speakerPower;
     }
 }
---
+++ src/StructuralPattern/Composite/Context/Speaker.java
@@ -0,0 +1,19 @@
+package StructuralPattern.Composite.Context;
+
+public class Speaker {
+    private int price;
+    private int power;
+
+    public Speaker(int power, int price){
+        this.power = power;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getPower() {
+        return power;
+    }
+}
