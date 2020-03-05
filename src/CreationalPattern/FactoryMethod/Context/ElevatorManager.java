66c5db8044fd10f66a871b7c3235f771c39edf2c
anylogic_국승현 <cooksh2421>
2020-03-05 10:58:32 +0900

    팩토리 메서드 복습중

--- src/CreationalPattern/FactoryMethod/Context/ElevatorManager.java
+++ src/CreationalPattern/FactoryMethod/Context/ElevatorManager.java
@@ -6,22 +6,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ElevatorManager {
+
     private List<ElevatorController> controllers;
     private ThroughputScheduler scheduler;
+
 
     /* 주어진 수만큼의 ElevatorController를 생성함 */
     public ElevatorManager(int controllerCount){
         // 엘리베이터의 이동을 책임지는 ElevatorController 객체 생성
-        controllers = new ArrayList<ElevatorController>(controllerCount);
+        controllers = new ArrayList<>(controllerCount);
 
         for(int i=0; i<controllerCount; i++){
-            ElevatorController controller = new ElevatorController(1);
+            ElevatorController controller = new ElevatorController(i);
             controllers.add(controller);
         }
 
         // 엘리베이터를 스케줄링(엘리베이터 선택)하기 위한 ThroughputScheduler 객체 생성
         scheduler = new ThroughputScheduler();
     }
+
 
     /* 요청에 따라 엘리베이터를 선택하고 이동시킴 */
     void requestElevator(int destination, Direction direction){
--- src/CreationalPattern/FactoryMethod/Context/ThroughputScheduler.java
+++ src/CreationalPattern/FactoryMethod/Context/ThroughputScheduler.java
@@ -4,6 +4,7 @@ import BehavioralPattern.TemplateMethod.Context.Enums.Direction;
 
 public class ThroughputScheduler {
     public int selectElevator(ElevatorManager manager, int destination, Direction direction){
+
         return 0;  // 임의로 선택함
     }
 }
