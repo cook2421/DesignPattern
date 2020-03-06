47a177fd9e54ba69b8ccadf936d5fbde4ebad77c
YI JAE EUN <yijaaan49@gmail.com>
2020-03-06 22:55:02 +0900

    pull

--- src/BehavioralPattern/TemplateMethod/Context/HyundaiMotor.java
+++ src/BehavioralPattern/TemplateMethod/Context/HyundaiMotor.java
@@ -13,18 +13,12 @@ public class HyundaiMotor {
         motorStatus = MotorStatus.STOPPED; //초기: 멈춘 상태
     }
 
-    private void moveHyundaiMotor(Direction direction){
-        // Hyundai Motor를 구동시킴
-        System.out.println("MOVING " + direction + "WARD");
-    }
-
     private void setMotorStatus(MotorStatus motorStatus) {
         this.motorStatus = motorStatus;
     }
     public MotorStatus getMotorStatus(){
         return motorStatus;
     }
-
 
     /* 엘리베이터 제어 */
     public void move(Direction direction){
@@ -33,15 +27,20 @@ public class HyundaiMotor {
 
         DoorStatus doorStatus = door.getDoorStatus();
 
-        // 만약 문이 열려 있으면 우선 문을 닫음
+        // step.1 만약 문이 열려 있으면 우선 문을 닫음
         if(doorStatus == DoorStatus.OPENED){
             door.close();
         }
 
-        // Hyundai 모터를 주어진 방향으로 이동시킴
+        // step.2 Hyundai 모터를 주어진 방향으로 이동시킴
         moveHyundaiMotor(direction);
 
-        // 모터 상태를 이동 중으로 변경함
+        // step.3 모터 상태를 이동 중으로 변경함
         setMotorStatus(MotorStatus.MOVING);
+    }
+
+    private void moveHyundaiMotor(Direction direction){
+        // Hyundai Motor를 구동시킴
+        System.out.println("MOVING " + direction + "WARD");
     }
 }
